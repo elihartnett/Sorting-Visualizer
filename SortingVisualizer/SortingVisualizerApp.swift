6d08e78714e4828b2a6b3442546ab86669a01d25
elichartnett <elichartnett@icloud.com>
2022-09-18 09:30:19 -0500

    Initial Commit

---
+++ SortingVisualizer/SortingVisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SortingVisualizerApp.swift
+//  SortingVisualizer
+//
+//  Created by Eli Hartnett on 9/18/22.
+//
+
+import SwiftUI
+
+@main
+struct SortingVisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
